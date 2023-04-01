e93aac73da7037b89e2358114e6e935e59c589e4
skateeveryday10 <falk.travis@gmail.com>
2023-04-01 13:32:51 -0400

    initial

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Footer from './Footer'
+
+export default function Layout({ children }) {
+  return (
+    <div className='Layout'>
+        <NavBar />
+          <main className="Content">
+              { children }
+          </main>
+        <Footer />
+    </div>
+  )
+}
