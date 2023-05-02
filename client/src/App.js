d70c2a29214a636170ee942ef9f1f2a56bbfd8fd
Travis-Laptop <falk.travis@gmail.com>
2023-05-02 10:57:44 -0400

    V0.0.1

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,41 @@ const supabase = createClient(
 function App(){
     return (
         <div className='App'>
-
-        </div>
+            <h1>Spatula Software</h1>
+            <div className='dashboardDisplay'>
+                <div className='userDisplay'>
+                    <img id='userPicture' />
+                    <p className='username'>Username</p>
+                    <p className='tier'>Basic Subscription</p>
+                </div>
+                <button id='changePlan'>Change Plan</button>
+            </div>
+            <div className='Pricing'>
+                <button>Anual</button>
+                <div className='monthlyPricingCards'>
+                        <div className='basic'>
+                            <p>25</p>
+                        </div>
+                        <div className='standard'>
+                            <p>35</p>
+                        </div>
+                        <div className='premium'>
+                            <p>50</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='anualPricingCards'>
+                    <div className='basic'>
+                        <p>25</p>
+                    </div>
+                    <div className='standard'>
+                        <p>35</p>
+                    </div>
+                    <div className='premium'>
+                        <p>50</p>
+                    </div>
+                </div>
+            </div>
     );
 }
  export default App;
