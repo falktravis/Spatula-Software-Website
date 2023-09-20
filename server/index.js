d231607aa0449fb88c501601f85162d8e2759e81
Anorak Haladay <falk.travis@gmail.com>
2023-09-20 16:31:44 -0400

    V1.0.9

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.Invites
+    GatewayIntentBits.DirectMessages
   ],
 });
 client.login(process.env.DISCORD_BOT_TOKEN);
